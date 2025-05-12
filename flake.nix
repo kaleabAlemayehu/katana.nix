9219f8e9afa0a9baaa86c8af18c238b7d99344bc
Kaleab Alemayehu (Neo) <cipherlofi04@gmail.com>
2025-05-12 09:39:41 -0400

    initial commit for katana

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+     home-manager = {
+       url = "github:nix-community/home-manager";
+       inputs.nixpkgs.follows = "nixpkgs";
+     };
+     xremap-flake.url = "github:xremap/nix-flake";
+     ghostty = {
+        url = "github:ghostty-org/ghostty";
+      };
+  };
+
+  outputs = { self, nixpkgs, ghostty, ... }@inputs: {
+    # use "nixos", or your hostname as the name of the configuration
+    # it's a better practice than "default" shown in the video
+    nixosConfigurations.katana = nixpkgs.lib.nixosSystem {
+      #extraSpecialArgs = {inherit inputs;};
+      specialArgs = {inherit inputs;};
+      # system= "x86_64-linux";
+      modules = [
+        ./hosts/default/configuration.nix
+        inputs.home-manager.nixosModules.default
+        ({ pkgs, ... }: {
+          environment.systemPackages = [
+            ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default
+          ];
+        })
+      ];};
+  };
+}
